ab009e45f4c1378a5bb69a8df0870edb6d0cb6bd
ignperez-udec <ignacio.perez.cerdeira@gmail.com>
2024-06-08 20:34:37 -0400

    initial commit

---
+++ app/src/main/java/com/ignacioperez/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,25 @@
+package com.ignacioperez.horoscapp.ui.horoscope
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ignacioperez.horoscapp.databinding.FragmentHoroscopeBinding
+
+class HoroscopeFragment : Fragment() {
+
+    //binding en fragment (no es view)
+    //_ al inicio del nombre significa privado
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
